356bcef53917a5191c77fac51a62e0a1f0771e12
liuhy-haplox <30582633+liuhy-haplox@users.noreply.github.com>
2017-08-24 16:56:29 +0800

    Add files via upload

---
+++ 20170824-1.py
@@ -0,0 +1,14 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:18:55) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> nums = [1,8,2,23,7,-4,18,23,2,1,37]
+>>> import heapq
+>>> heapq.heapify(nums)
+>>> nums
+[-4, 1, 1, 2, 7, 2, 18, 23, 23, 8, 37]
+>>> heapq.heappop(nums)
+-4
+>>> heapq.heappop(nums)
+1
+>>> heapq.heappop(nums)
+1
+>>> 
---
+++ 20170824-3.py
@@ -0,0 +1,12 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:18:55) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> data = b'Hello World'
+>>> data[0:5]
+b'Hello'
+>>> data.startswith(b'Hello')
+True
+>>> data.split()
+[b'Hello', b'World']
+>>> data.replace(b'Hello', b'Hello Better')
+b'Hello Better World'
+>>> 
